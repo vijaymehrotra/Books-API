a91d2225355a9b5299472f450cd01dfd1a571454
vijaymehrotra <vijaymehrotra99@gmail.com>
2024-08-23 07:03:15 +0530

    Added .gitignore

--- database/database.go
+++ database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	// "fmt"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -24,9 +25,9 @@ type Config struct {
 }
 
 func NewConnection(config Config) (*gorm.DB, error) {
-	// dsn := fmt.Sprintf("host=%s port=%s password=%s dbname=%s sslmode=%s user=%s",
-	// 	config.Host, config.Port, config.Password, config.DBName, config.SSLMode, config.User)
-	dsn := os.Getenv("POSTGRES_URL")
+	dsn := fmt.Sprintf("host=%s port=%s password=%s dbname=%s sslmode=%s user=%s",
+		config.Host, config.Port, config.Password, config.DBName, config.SSLMode, config.User)
+	// dsn := os.Getenv("POSTGRES_URL")
 
 	log.Println("Connecting to database...")
 	var db *gorm.DB
